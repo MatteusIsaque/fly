6b9d0ee06e95247da4b00d52d673a1560333ea90
Matteus Isaque <matteus.isaque28@gmail.com>
2023-11-01 13:12:04 -0300

    first commit

---
+++ src/app/politica-de-privacidade-e-termos-de-uso/page.tsx
@@ -0,0 +1,38 @@
+
+
+
+export default function Page() {
+
+  return (
+    <main>
+      <p>
+        Política de Privacidade:
+
+        Bem-vindo ao nosso serviço de assessoria habitacional. Valorizamos a sua confiança ao escolher utilizar os nossos serviços, e estamos comprometidos em proteger a sua privacidade. Esta Política de Privacidade explica como coletamos, usamos e protegemos as informações pessoais que você nos fornece. Ao utilizar os nossos serviços, você concorda com as práticas descritas nesta Política de Privacidade.
+
+        1. Informações que Coletamos:
+        Coletamos informações pessoais que você nos fornece ao usar nosso serviço de assessoria habitacional, incluindo nome, endereço de e-mail, número de telefone e outras informações necessárias para fornecer nossos serviços.
+
+        2. Uso das Informações:
+        Utilizamos as informações pessoais que coletamos para fornecer nossos serviços de assessoria habitacional, responder a suas perguntas, processar suas transações e fornecer suporte ao cliente. Não compartilhamos suas informações pessoais com terceiros sem o seu consentimento, exceto quando necessário para fornecer os serviços solicitados por você ou quando exigido por lei.
+
+        3. Segurança das Informações:
+        Implementamos medidas de segurança para proteger suas informações pessoais contra acesso não autorizado e uso indevido. No entanto, nenhum sistema de segurança é impenetrável, e não podemos garantir a segurança absoluta de suas informações.
+
+        Termos de Uso:
+
+        1. Uso do Serviço:
+        Ao utilizar nosso serviço de assessoria habitacional, você concorda em seguir todas as leis aplicáveis e a não violar os direitos de terceiros. Você concorda em utilizar o serviço apenas para fins legais e legítimos.
+
+        2. Conteúdo do Usuário:
+        Você é responsável por todo o conteúdo que você envia ou compartilha por meio do nosso serviço. Não nos responsabilizamos pelo conteúdo do usuário, mas reservamos o direito de remover ou modificar qualquer conteúdo que considerarmos inadequado ou violador destes Termos de Uso.
+
+        3. Limitação de Responsabilidade:
+        Nosso serviço é fornecido "no estado em que se encontra", e não fazemos garantias de qualquer tipo, expressas ou implícitas, sobre a precisão ou confiabilidade do serviço. Não seremos responsáveis por quaisquer danos diretos, indiretos, especiais, incidentais, consequenciais ou punitivos decorrentes do uso ou incapacidade de usar nosso serviço.
+
+        Ao utilizar nosso serviço, você concorda com esta Política de Privacidade e estes Termos de Uso. Reservamo-nos o direito de atualizar ou modificar esta Política de Privacidade e estes Termos de Uso a qualquer momento, sem aviso prévio. Recomendamos que você revise periodicamente esta página para estar ciente das alterações. Se tiver dúvidas sobre esta Política de Privacidade ou estes Termos de Uso, entre em contato conosco.
+
+        Data de Entrada em Vigor: 01 de novembro de 2023</p>
+    </main>
+  )
+}
